dc9816bd76718584c5cbc097caaf82ea7b214983
Gabriel Carmo <gabriel.carmo@rioita.mbr>
2019-08-29 21:16:40 -0300

    removendo funcoes inutilizadas

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,22 +1,5 @@
 const mongoose = require('mongoose')
 
-var optionalWithLength = function(minLength, maxLength) {
-	minLength = minLength || 0
-	maxLength = maxLength || Infinity
-	return {
-		validator : function(value) {
-			if (value === undefined) return true
-			return value.length >= minLength && value.length <= maxLength
-		},
-		message : 'Optional field is shorter than the minimum allowed length (' + minLength + ') or larger than the maximum allowed length (' + maxLength + ')'
-	}
-}
-
-var validateEmail = function(email) {
-	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-	return re.test(email)
-}
-
 const UserSchema = new mongoose.Schema(
 	{
 		email: {
